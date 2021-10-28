a68b51e10e568cab25fafcbdbe21e8883488242e
Szymekkkk <57588597+Szymekkkk@users.noreply.github.com>
2021-10-28 18:00:29 +0200

    Done

--- sketch_oct20a/sketch_oct20a.ino
+++ sketch_oct20a/sketch_oct20a.ino
@@ -9,11 +9,10 @@
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
-const int  buttonOK = 6;    
-const int  buttonS = 7;    
-const int  buttonM = 8;
-const int  buttonH = 9; 
+const int  buttonOK = 6;
 String pause = ":";
+bool lup = false;
+bool dclick = false;
 
 int buttonPushCounter = 0;
 int buttonState = 0;
@@ -38,35 +37,70 @@ void setup() {
   Timer1.attachInterrupt(timerIsr); 
   last = -1;
 }
-void button(int button, bool type,bool symbol){
+void button(int button){
   buttonState = button;
 
   if (buttonState != lastButtonState) {
     if (buttonState == HIGH) {
-      if(symbol){
-        if(type)
-          se++;
-        else
-          mi++;
-        }
-        else{
-         if(type)
-          se--;
-        else
-          mi--;
-        } 
-          }
-      
+      se++;  
     } 
     lastButtonState = buttonState;
+  }
 }
 
+void timerElapsing(){
+  while(!lup){
+    se--;
+  if(se < 0){
+    if(mi != 0){
+      se = 59;
+      mi--;
+    }
+    else
+      Alarm();
+    }
+    lcd.clear();
+    lcd.print("Timer:");
+    lcd.setCursor(0,1);
+    lcd.print(mi+pause+se);
+      delay(1000);
+  }
+
+}
+void Alarm(){
+  while(!dclick){
+      ClickEncoder::Button b = encoder->getButton();
+  if (b != ClickEncoder::Open) {
+    switch (b) {
+      case ClickEncoder::DoubleClicked:
+         dclick = true;
+         lup = true;
+      break;        
+    }
+  }
+    se = 0;
+    lcd.clear();
+    lcd.print("Alarm!");
+    lcd.setCursor(0,1);
+    lcd.print("Press twice!");
+    delay(50);
+  }
+  }
 
 void loop() {
-  button(digitalRead(buttonOK),true,true);
-    
+  button(digitalRead(buttonOK));
+  ClickEncoder::Button b = encoder->getButton();
+  if (b != ClickEncoder::Open) {
+    switch (b) {
+      case ClickEncoder::Clicked:
+        timerElapsing();
+        dclick = false;
+        lup = false;
+      break;    
+    }
+  }
   se += encoder->getValue();
-  if(se == 60){
+  if(se >= 60){
     se = 0;
     mi++;
   }
@@ -82,23 +116,12 @@ void loop() {
 
   if (se != last) {
     lcd.clear();
-    lcd.print("Minutnik:");
+    lcd.print("Timer:");
     lcd.setCursor(0,1);
     lcd.print(mi+pause+se);
     last = se;
   }
 
-  ClickEncoder::Button b = encoder->getButton();
-  if (b != ClickEncoder::Open) {
-    switch (b) {
-      case ClickEncoder::Clicked:
-        digitalWrite(LED_BUILTIN, HIGH);
-      break;  
-      case ClickEncoder::DoubleClicked:
-         digitalWrite(LED_BUILTIN, LOW);
-      break;        
-    }
-  }
-  lcd.setCursor(0, 1);
+
   delay(50);
   }
